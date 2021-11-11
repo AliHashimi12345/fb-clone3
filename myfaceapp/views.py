6f8959293dd68eb2a09667dc03e11dbba35e5a39
AliHashimi12345 <alihashimivns@gmail.com>
2021-11-11 08:39:58 +0530

    second commit

--- myfaceapp/views.py
+++ myfaceapp/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http.response import HttpResponse
 from django.shortcuts import render,redirect
 from django.contrib.auth import login ,logout,authenticate
